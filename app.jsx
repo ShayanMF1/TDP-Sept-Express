54d40be5c524cb502ecf56a6302ebd41ac7a5a92
ShayanMF1 <shayan1227@gmail.com>
2022-10-19 12:00:46 +0100

    react

--- app.jsx
+++ app.jsx
@@ -1,30 +1,73 @@
 import './App.css';
-import Person from './Person';
-import people from './people.json';
+// import Person from './Person';
+// import Products from './Components/Props/Products';
+// import Parent from './Components/Props/Parent';
+// import TrueFalse from './Components/States/TrueFalse';
+// import Counter from './Components/States/Counter';
+// import Converter from './Components/States/Converter';
+// import Person from './Components/Static-Data/Person';
+// import people from './Components/Static-Data/people.json';
+// import Poke from './Components/Data-Requests/Poke';
+import {
+  BrowserRouter as Router, Link, Route, Routes,
+} from 'react-router-dom';
+import Counter from './Components/States/Counter';
+// import OMDB from './Components/Data-Requests/OMDB';
+import SearchableList from './Components/Lifting-State/SearchableList';
+import Home from './Components/Routing/Home';
+import Navigation from './Components/Routing/Navigation';
+import ParamsExample from './Components/Routing/ParamsExample';
 
 function App() {
-
-    const Heading = () => <h1>Test</h1>
-
-    const Me = (props) => {
-        console.log("PROPS:", props);
-      return ( 
-        <>
-            <p>Name: {props.name}</p>
-            <p>Age: {props.age}</p>
-            <p>Height: {props.height}</p>
-        </>
-)};
-return (
-    <div classname="App">
-        {
-            people.map(person => {
-                console.log("PERSON:", person);
-                return <Person key={person.name + person.age} name ={person.age} job={person.job}/>;
-                })
-            }
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <header>
+          <h1>I AM A HEADER</h1>
+        </header>
+        <nav>
+          <Link to="/home">
+            Home
+          </Link>
+          <Link to="/liftingState">
+            <button type="button">Lifting State</button>
+          </Link>
+          <Link to="/state">
+            State
+          </Link>
+          <Navigation />
+        </nav>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/state" element={<Counter />} />
+          <Route path="/liftingState" element={<SearchableList />} />
+          <Route path="/params/:colour" element={<ParamsExample />} />
+        </Routes>
+        <footer>
+          I AM A FOOTER
+        </footer>
+      </Router>
+      {/* <Me name="Jordan" age={28} job="Trainer"/> */}
+      {/* {Me({ name: "JH", age: 28, job: "Trainer"})} */}
+      {/* {Heading()} */}
+      {/* <Heading/> */}
+      {/* <Converter/> */}
+      {/* <TrueFalse/> */}
+      {/* <Counter/> */}
+      {/* {
+        people.map(person => {
+          console.log("PERSON:", person);
+          return <Person
+          key={person.name + person.age}
+          name={person.name}
+          age={person.age}
+          job={person.job}/>;
+        })
+      } */}
+      {/* <Poke /> */}
+      {/* <OMDB /> */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/searchablelist.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable no-debugger */
+import { useState } from 'react';
+import { v4 } from 'uuid';
+import Item from '../../Item';
+import SearchBar from './SearchBar';
+
+function SearchableList() {
+  const [items, setItems] = useState([{
+    name: 'milk',
+    quantity: 1,
+  }, {
+    name: 'eggs',
+    quantity: 1,
+  }, {
+    name: 'bread',
+    quantity: 1,
+  }]);
+  const [search, setSearch] = useState('');
+  const [newItem, setNewItem] = useState('');
+
+  const removeItem = (index) => {
+    const cloneItems = [...items];
+    cloneItems.splice(index, 1);
+    setItems(cloneItems);
+  };
+
+  const changeQuantity = (index, changeInQuantity) => {
+    const cloneItems = [...items];
+    cloneItems[index].quantity += changeInQuantity;
+    if (cloneItems[index].quantity === 0) return removeItem(index);
+    return setItems(cloneItems);
+  };
+
+  const addNewItem = (event) => {
+    event.preventDefault();
+    const cloneItems = [...items]; // ?
+    for (let i = 0; i < cloneItems.length; i += 1) {
+      if (cloneItems[i].name === newItem) {
+        cloneItems[i].quantity += 1;
+        return setItems(cloneItems);
+      }
+    }
+
+    setItems((currentItems) => [...currentItems, { name: newItem, quantity: 1 }]);
+    return setNewItem('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={addNewItem}>
+        <label htmlFor="newItem">
+          New Item:
+          <input type="text" id="newItem" value={newItem} onChange={(e) => setNewItem(e.target.value)} />
+        </label>
+        <button type="submit">Add New</button>
+      </form>
+      <br />
+      <SearchBar text={search} changeHandler={(e) => setSearch(e.target.value)} />
+      {
+            items
+              .filter(({ name }) => name.toLowerCase().startsWith(search.toLowerCase()))
+              .map(({ name, quantity }, index) => (
+                <Item
+                  key={v4()}
+                  name={name}
+                  quantity={quantity}
+                  handleClick={() => removeItem(index)}
+                  changeQuantity={(change) => changeQuantity(index, change)}
+                />
+              ))
+        }
+    </div>
+  );
+}
+
+export default SearchableList;
